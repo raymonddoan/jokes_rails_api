ba9d124fee399b3ba34f050d4cddb2ae7b7eb3ea
whywesmurfing <raymond.doan94@gmail.com>
2021-06-20 23:05:30 +1000

    Created full CRUD functionality for Jokes API

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,7 +1,51 @@
 class JokesController < ApplicationController
+  before_action :set_joke, only: [:show, :update, :destroy]
+
   def index
     @jokes = Joke.all
     render json: @jokes
   end
   
+  def create
+    @joke = Joke.create(joke_params)
+    if @joke.errors.any?
+      render json: @joke.errors, status: :unprocessable_entity # fails to meet validation
+    else
+      render json: @joke, status: 201
+    end
+  end
+
+  def show
+    render json: @joke
+  end
+
+  def update
+    @joke.update(joke_params)
+    if @joke.errors.any?
+      render json: @joke.errors, status: :unprocessable_entity # fails to meet validation
+    else
+      render json: @joke, status: 201
+    end
+  end
+  
+  def destroy
+    @joke.delete
+    render json: 204
+  end
+  
+  
+  private
+  def joke_params
+    params.require(:joke).permit(:category_id, :body)
+  end
+  
+  def set_joke
+    begin
+      @joke = Joke.find(params[:id])
+    rescue => exception
+      render json: {error: "Joke Not found"}, status: 404 
+    end
+  end
+  
+
 end
